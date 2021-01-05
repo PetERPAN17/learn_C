38591b1a97cb4d0640e9e8a2a3912ed97dd50bb4
PetERPAN17 <peterpan17.dev@gmail.com>
2021-01-05 17:24:15 +0900

    2020-01-05 17:23

--- 001_start_solution/001_start/001_start.c
+++ 001_start_solution/001_start/001_start.c
@@ -1,15 +1,35 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-#include <stdlib.h>
-
-#define PI 3.131592f
 
 int main()
 {
-	char c, d;
-	scanf("%c,%c", &c, &d);
-	printf("%i %i\n", c, d);
+	int a = 0;
+	a++;
+	printf("%d\n", a);
+
+	++a;
+	printf("%d\n", a);
+
+	double b = 0;
+	b++;
+	printf("%f\n", b);
+
+	++b;
+	printf("%f\n", b);
+
+	int count = 0;
+	while (++count < 10)
+	{
+		printf("%d ", count);
+	}
+
+	printf("\n");
+	int count2 = 0;
+	while (count2++ < 10)
+	{
+		printf("%d ", count2);
+	}
 
 	return 0;
 }
