5795dbfb412f88f9227343383dd97d085488621d
PetERPAN17 <peterpan17.dev@gmail.com>
2021-01-05 16:57:14 +0900

    first commit

---
+++ 001_start_solution/001_start/001_start.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.131592f
+
+int main()
+{
+	char c, d;
+	scanf("%c,%c", &c, &d);
+	printf("%i %i\n", c, d);
+
+	return 0;
+}
