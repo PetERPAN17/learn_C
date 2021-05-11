4a430db86819a36c8aeae2e65eb26666cc525e5c
PetERPAN17 <peterpan17.dev@gmail.com>
2021-05-12 08:48:17 +0900

    Add #21

---
+++ 7.12_solution/7.12/main.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	char c;
+
+	while ((c = getchar()) != '.')
+	{
+		printf("You love ");
+
+		switch (c)
+		{
+		case 'a':
+		case 'A':
+			printf("apple.");
+			break;
+		case 'b':
+		case 'B':
+			printf("baseball.");
+			break;
+		case 'c':
+		case 'C':
+			printf("cake.");
+			break;
+		default:
+			printf("nothing.");
+		}
+
+		printf("\n");
+
+		while (getchar() != '\n')
+			continue;
+	}
+
+	return 0;
+}
