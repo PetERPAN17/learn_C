a4b933f8ee9bde82474a5d6a628a1799bcaeba1b
PetERPAN17 <peterpan17.dev@gmail.com>
2021-05-20 08:09:16 +0900

    Add #24

---
+++ 8.5_solution/8.5/main.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void display(char cr, int lines, int width);
+
+int main()
+{
+	char c;
+	int rows, cols;
+
+	//while (1)
+	//{
+	//	scanf("%c %d %d", &c, &rows, &cols);
+	//	while (getchar() != '\n') continue;
+	//	display(c, rows, cols);
+	//	if (c == '\n')
+	//		break;
+	//}
+
+	printf("Input one character and two integers:\n");
+	while ((c = getchar()) != '\n')
+	{
+		scanf("%d %d", &rows, &cols);
+		while (getchar() != '\n') continue;
+		display(c, rows, cols);
+		printf("Input another chracter and two integers:\n");
+		printf("Press Enter to quit.\n");
+	}
+
+	return 0;
+}
+
+void display(char cr, int lines, int width)
+{
+	for (int i = 1; i <= lines; ++i) {
+		for (int j = 1; j <= width; ++j) {
+			printf("%c", cr);
+		}
+		printf("\n");
+	}
+}
